cffb55fe5f678ed1a56108c61ff84f59135ce894
Chou4J <jasonchou@foyatech.com>
2017-08-08 14:01:38 +0800

    + JsonHelper.swift

---
+++ IOS/JsonExample/JsonHelper.swift
@@ -0,0 +1,48 @@
+//
+//  JsonHelper.swift
+//  取得ClassType 參考於 : https://stackoverflow.com/questions/40373030/how-to-create-instance-of-a-class-from-a-string-in-swift-3
+//
+
+import Foundation
+
+class JsonHelper
+{
+    public func JsonStringtoJsonObject(jsonString:String,isJsonArray:Bool,cls:String)->  AnyObject  {
+        
+        let myclass = stringClassFromString(cls) as! BaseModel.Type;
+        
+        if(isJsonArray)
+        {
+            let jsonData = jsonString.data(using: String.Encoding.utf8, allowLossyConversion: false)
+            let json = try? JSONSerialization.jsonObject(with: jsonData!, options: []) as! [[String: AnyObject]];
+            
+            var objList : [AnyObject] = [];
+            
+            for one in json! {
+                let oneCls = myclass.init(str: nil,obj: one);
+                oneCls.SetJsonClassInit_FromObj();
+                objList.append( oneCls );
+            }
+            return objList as AnyObject;
+        }
+        else
+        {
+            let res = myclass.init(str: jsonString,obj:nil);
+            res.SetJsonClassInit();
+            return res ;
+        }
+        
+    }
+    
+    func stringClassFromString(_ className: String) -> AnyClass! {
+        
+        /// get namespace
+        let namespace = Bundle.main.infoDictionary!["CFBundleExecutable"] as! String;
+        
+        /// get 'anyClass' with classname and namespace
+        let cls: AnyClass = NSClassFromString("\(namespace).\(className)")!;
+        
+        // return AnyClass!
+        return cls;
+    }
+}
